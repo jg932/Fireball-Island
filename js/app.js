72061badcfb3f42ab80607aa16e420efafeae276
jg932 <jared.h.goldstein@gmail.com>
2021-10-18 22:22:14 -0400

    whos rollDie function works

--- js/app.js
+++ js/app.js
@@ -85,16 +85,25 @@ playerFour = {
 }
 
 players = [playerOne, playerTwo, playerThree, playerFour]
+turnIndex = 0
+// playerOneMissTurn = [playerTwo, playerThree, playerFour, playerTwo, playerThree, playerFour, RESUME?]
+// playerTwoMissTurn = [playerThree, playerFour, playerOne, playerThree, playerFour, playerOne, playerTwo, playerThree, playerFour, RESUME?]
+// playerThreeMissTurn = [playerFour, playerOne, playerTwo, playerFour, RESUME?]
+// playerFourMissTurn = [playerOne, playerTwo, playerThree, RESUME?]
 
 /*-------------------------------- Cached Elements ---------------------------*/
-const playerOne = document.querySelector("#player1")
-const playerTwo = document.querySelector("#player2") 
-const playerThree = document.querySelector("#player3")
-const playerFour = document.querySelector("#player4")
+// const playerOne = document.querySelector("#player1")
+// const playerTwo = document.querySelector("#player2") 
+// const playerThree = document.querySelector("#player3")
+// const playerFour = document.querySelector("#player4")
 const die = document.querySelector("#dice")
 const diceDisplay = document.querySelector("#dice-display")
 const whosTurnDisplay = document.querySelector("#whos-turn-display")
 const cardSquaresDivs = document.querySelector(".card-sqaures")
+const allSquares = document.querySelector(".squares")
+const treasureSquare = document.querySelector(".treasure")
+const welcomeSquare = document.querySelector(".welcome")
+const finishSquare = document.querySelector(".finish")
 
 /*-------------------------------- Event Listeners ---------------------------*/
 die.addEventListener("click", rollAndMove)
@@ -105,30 +114,52 @@ die.addEventListener("click", rollAndMove)
 /*-------------------------------- Functions ---------------------------*/
 init()
 function init(){
-  turnUpdate()
+  players.forEach(player => {
+    player.position = 0
+  });
+  currentTurn = players[0]
+  render()
 }
 
 function turnUpdate (){
-  let index = 0;
-  currentTurn = players[index];
-  // console.log(currentTurn)
-  index++;
-  currentTurn = players[index];
-  // console.log(currentTurn);
+  console.log(turnIndex)
+  if (turnIndex >= 3) {
+    turnIndex = 0
+  } else {
+    turnIndex++
   }
+  currentTurn = players[turnIndex];
+  render()
+}
 
 function rollAndMove(){
   // console.log("here")
   rollResult = generateRoll();
-  let activePlayerPosition = currentTurn.player.position
-  document.querySelector(`#sq${activePlayerPosition}`).innerHTML = ""
-  activePlayerPosition += rollResult
-   // console.log("here2")
-  // whosTurn = "P1"
-  diceDisplay.innerHTML = `${rollResult}`
-  document.querySelector(`#sq${activePlayerPosition}`).innerHTML = `${currentTurn}`
+  currentTurn.position += rollResult
   turnUpdate()
+  render()
 }
+
+function render() {
+  allSquares.innerHTML = ""
+  diceDisplay.innerHTML = `${rollResult}`
+  whosTurnDisplay.innerHTML = `It is ${currentTurn.name}'s turn!`
+  // document.querySelector(`#sq${currentTurn.position}`).innerHTML = ""
+  // document.querySelector(`#sq${currentTurn.position}`).innerHTML += currentTurn.name
+  // console.log(`#sq${players[0].position}`)
+  players.forEach((player) => document.querySelector(`#sq${player.position}`).innerHTML += `${player.name}`)
+  welcomeSquare.innerHTML = "Welcome"
+  treasureSquare.innerHTML = "Treasure"
+  finishSquare.innerHTML = "Finish"
+}
+
+//// create list of all squares
+//// clear all squares at start of render function
+//// restore Welcome Treasure and Finish squares
+// then render looping through players array for all positions
+//append all positions instead of "innerHTML" (+=)
+
+
 
 function generateRoll(){
   min = Math.ceil(1);
@@ -139,9 +170,14 @@ function generateRoll(){
 function drawCard(){
   if (playerOne.position || playerTwo.position || playerThree.position || playerFour.position === cardSquaresDivs){
     function cardNumberGenerator(){ 
+    cardIndex = 0
     min = Math.ceil(0);
-    max = Math.floor(6);
-    return Math.floor(Math.random() * (max - min + 1) + min)
+    max = Math.floor(51);
+    cardIndex = Math.floor(Math.random() * (max - min + 1) + min)
     }
   }
+}
+
+function checkforTreasureSquare(){
+
 }
